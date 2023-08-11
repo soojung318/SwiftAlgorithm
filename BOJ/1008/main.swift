dad1d4e9ae0ed2896c81ff667fd9fd0590706fc7
Chris <sjshin0318@gmail.com>
2023-08-12 01:54:40 +0900

    Update Swift Algorithm

---
+++ BOJ/1008/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  1008
+//
+//  Created by chris on 2023/08/12.
+//
+
+import Foundation
+
+let ipt = readLine()!
+let iptArr = ipt.split(separator: " ")
+let a = Double(iptArr[0])!
+let b = Double(iptArr[1])!
+print(a/b)
