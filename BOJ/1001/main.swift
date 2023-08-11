4ce879c5470ae37f30c2873f87fb11345b819d5f
Chris <sjshin0318@gmail.com>
2023-08-12 01:20:08 +0900

    Update Swift Algorithm

---
+++ BOJ/1001/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  1001
+//
+//  Created by chris on 2023/08/12.
+//
+
+import Foundation
+
+//print("숫자 두개를 입력해 주세여")
+let input1 = readLine()!
+let lineArr1 = input1.split(separator: " ")
+let a1 = Int(lineArr1[0])!
+let b1 = Int(lineArr1[1])!
+print(a1-b1)
---
+++ BOJ/10998/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  10998
+//
+//  Created by chris on 2023/08/12.
+//
+
+import Foundation
+
+let input = readLine()!
+let inputArr = input.split(separator: " ")
+let a = Int(inputArr[0])!
+let b = Int(inputArr[1])!
+print(a*b)
+
+
