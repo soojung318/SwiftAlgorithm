bf7fda43d349292be8e2a226b78ce6f0f2a487d4
Chris <sjshin0318@gmail.com>
2023-08-12 11:24:59 +0900

    Update Swift Algorithm Study

---
+++ 10926/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  10926
+//
+//  Created by chris on 2023/08/12.
+//
+
+import Foundation
+
+let id = readLine()!
+print(id+"??!")
+
---
+++ BOJ/10869/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  10869
+//
+//  Created by chris on 2023/08/12.
+//
+
+import Foundation
+
+let input = readLine()!
+let inputArr = input.split(separator: " ")
+let a = Int(inputArr[0])!
+let b = Int(inputArr[1])!
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+
