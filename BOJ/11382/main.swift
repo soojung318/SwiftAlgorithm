6e749b9132fe4aaca8619564214783858c5058ca
Chris <sjshin0318@gmail.com>
2023-08-13 20:44:46 +0900

    Update

---
+++ BOJ/11382/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  11382
+//
+//  Created by chris on 2023/08/13.
+//
+
+import Foundation
+
+let input = readLine()!
+let inputArr = input.split(separator: " ")
+let a = Int(inputArr[0])!
+let b = Int(inputArr[1])!
+let c = Int(inputArr[2])!
+print(a+b+c)
