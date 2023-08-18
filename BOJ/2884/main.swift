ddcf9eda6d62523f9bfbe467fe43af502166bf94
Chris <sjshin0318@gmail.com>
2023-08-18 14:47:54 +0900

    Update BOJ

--- BOJ/2884/main.swift
+++ BOJ/2884/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
+let input = readLine()!
+let inputArr = input.split(separator: " ")
+var h = Int(inputArr[0])!
+var m = Int(inputArr[1])!
 
+// h=0,m>60
+if h==0 {
+    h = 23
+    if m < 60 {
+        m -= 45
+    }
+    print(h)
+}
+
+print()
---
+++ test1/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  test1
+//
+//  Created by chris on 2023/08/16.
+//
+
+import Foundation
+
+
