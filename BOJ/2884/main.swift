e80a8939c07e3d5232d07d1425ef63c880f40973
Chris <sjshin0318@gmail.com>
2023-08-19 13:17:06 +0900

    Updated BOJ

--- BOJ/2884/main.swift
+++ BOJ/2884/main.swift
@@ -7,18 +7,28 @@
 
 import Foundation
 
-let input = readLine()!
-let inputArr = input.split(separator: " ")
-var h = Int(inputArr[0])!
-var m = Int(inputArr[1])!
+//let input = readLine()!
+//let inputArr = input.split(separator: " ")
+//var h = Int(inputArr[0])!
+//var m = Int(inputArr[1])!
+//
+//
+//m -= 45
+//
+//if h < 0 {
+//    h +=  24
+//    print("\(h) \(m)")
+//}
+//if m < 0 {
+//    h -= 1
+//    m += 60
+//    print("\(h) \(m)")
+//}
 
-// h=0,m>60
-if h==0 {
-    h = 23
-    if m < 60 {
-        m -= 45
-    }
-    print(h)
-}
+let input = readLine()!.split(separator: " ").map{Int($0)!}
 
-print()
+let hour = input[0]
+let min = input[1]
+
+let time = hour * 60 + min - 45 < 0 ? 60 * 24 + min - 45 : hour * 60 + min - 45
+print(time / 60, time % 60)
