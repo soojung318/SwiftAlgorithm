36d0ef51bc71152baf645984f6db789f68798277
Chris <sjshin0318@gmail.com>
2023-08-25 11:54:46 +0900

    Update swift algorithms

--- Exercise/isPrime/main.swift
+++ Exercise/isPrime/main.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+
+//-----------------1------------------
 let num = Int(readLine()!)!
 var isPrime: Bool = true
 
@@ -23,3 +25,36 @@ if isPrime {
 } else{
     print("It is not a prime number.")
 }
+
+
+//-----------------2------------------
+func primeNumberCheck(num: Int) -> Bool {
+    for i in 2..<num{
+        if num % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+primeNumberCheck(num: 7)
+
+if primeNumberCheck(num: 7) {
+    print("It is a prime number.")
+} else {
+    print("It is not a prime number.")
+}
+
+
+//-----------------3------------------
+func primeNumber(num: Int) {
+    for i in 2..<num {
+        if num % i == 0 {
+            print("It is not a prime number.")
+        } else {
+            print("It is a prime number.")
+        }
+    }
+}
+
+primeNumber(num: 97)
