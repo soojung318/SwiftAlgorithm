8f084f03f4b338c33f5b4c0313654ce8c732a1cb
Chris <sjshin0318@gmail.com>
2023-09-21 23:48:47 +0900

    Update no.8393 of BOJ

---
+++ BOJ/Step03-Loops/8393/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  8393
+//
+//  Created by chris on 2023/09/21.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+var sum = 0
+for i in 1...n {
+    sum += i
+}
+print(sum)
+
