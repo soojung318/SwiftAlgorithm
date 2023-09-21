18d26e699f804e2fdb42601bf5feb8ffc5d3d13c
Chris <sjshin0318@gmail.com>
2023-09-21 23:51:47 +0900

    Update no.25314 of BOJ

---
+++ BOJ/Step03-Loops/25314/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  25314
+//
+//  Created by chris on 2023/09/21.
+//
+
+import Foundation
+
+let N = Int(readLine()!)! / 4
+for _ in 1...N {
+    print(terminator: "long ")
+}
+print("int")
+
